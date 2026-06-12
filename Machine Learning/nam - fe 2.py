ecf66c279c63fcd05624c892fc7784f3d74617df
31231021229@ueh.edu.vn <hanhly.31231021229@st.ueh.edu.vn>
2026-06-12 16:28:22 +0700

    deleted pearson correlation in na - fe

--- Machine Learning/nam - fe 2.py
+++ Machine Learning/nam - fe 2.py
@@ -25,51 +25,10 @@ df = df.withColumn("HumidityDiff", col("Humidity9am") - col("Humidity3pm"))
 df = df.withColumn("PressureDiff", col("Pressure9am") - col("Pressure3pm"))
 df = df.withColumn("WindSpeedDiff", col("WindSpeed3pm") - col("WindSpeed9am"))
 
-
-
-
-# ==========================================
-# PHẦN 2.5: PEARSON CORRELATION (EDA)
-# ==========================================
-print("Đang tính Pearson correlation với RainTomorrow...")
-
-# Encode target (RainTomorrow -> 0/1)
-df_corr = df.withColumn(
-    "RainTomorrow_num",
-    when(col("RainTomorrow") == "Yes", 1).otherwise(0)
-)
-
-# Chọn numeric features để EDA
-eda_features = [
-    "MinTemp", "MaxTemp", "Rainfall", "TempRange",
-    "Humidity9am", "Humidity3pm", "HumidityDiff",
-    "Pressure9am", "Pressure3pm", "PressureDiff",
-    "WindGustSpeed", "WindSpeed9am", "WindSpeed3pm", "WindSpeedDiff"
-]
-
-assembler_eda = VectorAssembler(
-    inputCols=eda_features + ["RainTomorrow_num"],
-    outputCol="eda_features"
-)
-
-df_eda_vector = assembler_eda.transform(df_corr).select("eda_features")
-
-corr_matrix = Correlation.corr(df_eda_vector, "eda_features", "pearson").head()[0]
-
-# Convert sang pandas để đọc dễ hơn
-corr_array = corr_matrix.toArray()
-cols = eda_features + ["RainTomorrow_num"]
-
-corr_df = pd.DataFrame(corr_array, columns=cols, index=cols)
-
-print("\n===== CORRELATION WITH RainTomorrow =====")
-print(corr_df["RainTomorrow_num"].sort_values(ascending=False))
-
-
 # ==========================================
 # PHẦN 3: VECTOR ASSEMBLER (ML PIPELINE)
 # ==========================================
-print("Đang gom nhóm 22 biến độc lập...")
+print("Đang gom nhóm biến độc lập...")
 
 feature_cols = [
     "MinTemp", "Temp3pm", "TempRange",
